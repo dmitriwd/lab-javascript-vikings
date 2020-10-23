9086891d649347d9e42815fc40dcf6f354daafd7
dmitriwd <dmitri.net@protonmail.com>
2020-10-23 17:49:25 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,42 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+    constructor (health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    
+    receiveDamage(damage){
+        this.health = this.health - damage;
+    }
+    attack (){
+        return this.strength;
+    };
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    };
+    
+    receiveDamage(damage) {
+        this.health > 0 ? `${this.name} has received ${this.damage} 
+        points of damage` :
+            `${this.name} has died in act of combat`;
+    };
 
+    battleCry() {
+        return 'Odin Owns You All!';
+    }
+};
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    super();
+    receiveDamage(damage) {
+        this.health > 0 ? `${this.name} has received ${this.damage} 
+        points of damage` :
+            `${this.name} has died in act of combat`;
+}
 
 // War
 class War {}
